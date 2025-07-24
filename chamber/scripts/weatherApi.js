76c5cf5eb8fd1f7f72b2f79c8dc4b318cc7a08d9
Destiny Linus <youngdestinx@gmail.com>
2025-07-24 12:21:52 +0100

    done

--- chamber/scripts/weatherApi.js
+++ chamber/scripts/weatherApi.js
@@ -3,11 +3,11 @@ const graphic = document.querySelector("#graphic");
 const description = document.querySelector("#description");
 const temperature = document.querySelector("#temperature");
 
-const myKey = "8263f8e8332a3d9567fe5ed984ed6084";
-const lat = "6.463253448638566";
-const long = "3.144348686586198";
+const myKey = "8263f8e8332a3d9567fe5ed984ed6084"
+const lat = "6.463253448638566"
+const long = "3.144348686586198"
 
-const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myKey}&units=imperial`;
+const url = `//api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${myKey}&units=imperial`
 
 async function apiFetch() {
     try {
@@ -15,6 +15,7 @@ async function apiFetch() {
         if (response.ok) {
             const data = await response.json();
             console.log(data);
+            displayResult(data);
         } else {
             throw Error(await response.text());
         }
@@ -23,3 +24,12 @@ async function apiFetch() {
     }
 }
 apiFetch();
+
+function displayResult(data) {
+    town.innerHTML = data.name
+    description.innerHTML = data.weather[0].description
+    temperature.innerHTML = `${data.main.temp}⁰F`
+    const iconsrs = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    graphic.setAttribute("src", iconsrs)
+    graphic.setAttribute("alt", data.weather[0].description)    
+}
