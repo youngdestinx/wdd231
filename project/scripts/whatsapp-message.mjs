733ad5a37ac3d3c44c58a43f25f4f0deb2a5eb8a
Destiny Linus <youngdestinx@gmail.com>
2025-08-11 10:32:50 +0100

    whatsapp messages formatted

--- project/scripts/whatsapp-message.mjs
+++ project/scripts/whatsapp-message.mjs
@@ -7,8 +7,7 @@ function getMessage() {
     const packages = params.get('package');
 
     const whatsappmessage = `
-        Hello. My name is  ${fname.charAt(0).toUpperCase() + fname.slice(1)} ${lname.charAt(0).toUpperCase() + lname.slice(1)}. I have just registered for the ${packages}
-        I will love get full description, make payment and get started. Alternatively, you can also contact me at ${email}
+        Hello. My name is  ${fname.charAt(0).toUpperCase() + fname.slice(1)} ${lname.charAt(0).toUpperCase() + lname.slice(1)}. I have just registered for the ${packages} I will love get full description, make payment and get started. Alternatively, you can also contact me at ${email}
     `
 
     const whatsappLink = `https://wa.me/2349152680996?text=${whatsappmessage}`;
